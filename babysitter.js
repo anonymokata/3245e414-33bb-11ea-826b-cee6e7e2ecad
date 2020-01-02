955c00c6d8b69ab530b46f306e9afab4e94f228e
someone <someone>
2020-01-02 18:29:08 -0500

    Added startsBeforeFinishes function and created tests for it.

--- babysitter.js
+++ babysitter.js
@@ -9,13 +9,23 @@ class Babysitter {
   isWorkingHours(startTime, endTime){
     let workingHours = false;
     // convert string times into ints for internal time
-    // TODO decide if conversion should happen earlier and those values will be passed
     startTime = convertToInternalTime(startTime);
     endTime = convertToInternalTime(endTime);
     if ((startTime >= this.earliestStartTime || startTime <= this.latestEndTime) 
       && (endTime >= this.earliestStartTime || endTime <= this.latestEndTime))
       workingHours = true;
     return workingHours;
+  }
+  startsBeforeFinishes(startTime, endTime){
+    let startMeridiem = startTime.slice(-2).toLowerCase();
+    let endMeridiem = endTime.slice(-2).toLowerCase();
+    if (endMeridiem === 'pm' && startMeridiem === 'am')
+      return false;
+    else if (endMeridiem === startMeridiem)
+      // convert string times into ints for internal time. If the end value is larger, that means it started later
+      return convertToInternalTime(startTime) < convertToInternalTime(endTime);
+    else
+      return true;
   }
 }
 
--- tests/babysitter.test.js
+++ tests/babysitter.test.js
@@ -2,7 +2,7 @@ const Babysitter = require("../babysitter");
 
 let babysitter = new Babysitter;
 
-describe("Check if hours are appropriate working hours", () => {
+describe("Check if hours are appropriate working hours (no earlier than 5pm and no later than 4am)", () => {
   test("Starting at 5pm and ending at 4am returns true", () => {
     expect(babysitter.isWorkingHours("5pm","4am")).toBe(true);
   });
@@ -23,4 +23,30 @@ describe("Check if hours are appropriate working hours", () => {
     expect(babysitter.isWorkingHours("5:40pm","4:02am")).toBe(false);
   });
  
+});
+
+describe("Check if starting hours are before ending hours", () => {
+  test("Starting at 5pm and ending at 4am returns true", () => {
+    expect(babysitter.startsBeforeFinishes("5pm","4am")).toBe(true);
+  });
+
+  test("Starting at 12am and ending at 3am returns true", () => {
+    expect(babysitter.startsBeforeFinishes("5pm","4am")).toBe(true);
+  });
+
+  test("Starting at 1am and ending at 3am returns true", () => {
+    expect(babysitter.startsBeforeFinishes("1am","3am")).toBe(true);
+  });
+
+  test("Starting at 12am and ending at 10pm returns false", () => {
+    expect(babysitter.startsBeforeFinishes("12am","10pm")).toBe(false);
+  });
+
+  test("Starting at 12am and ending at 12am returns false", () => {
+    expect(babysitter.startsBeforeFinishes("12am","12am")).toBe(false);
+  });
+
+  test("Starting at 8pm and ending at 6pm returns false", () => {
+    expect(babysitter.startsBeforeFinishes("8pm","6pm")).toBe(false);
+  });
 });
