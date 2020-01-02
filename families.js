bdc101c9064e3ed6b66d6dbdebfd7e6d6a15a594
someone <someone>
2020-01-02 18:48:03 -0500

    Added shell of family class and created the three families.

---
+++ families.js
@@ -0,0 +1,40 @@
+const Family = require("./Family");
+
+// Instances of all the families for the babysitter to keep track of.
+
+const familyA = new Family([
+  {
+    "range": "5pm-11pm",
+    "rate": 15
+  },
+  {
+    "range": "11pm-4am",
+    "rate": 20,
+  }
+]);
+
+const familyB = new Family([
+  {
+    "range": "5pm-10pm",
+    "rate": 12
+  },
+  {
+    "range": "10pm-12am",
+    "rate": 8,
+  },
+  {
+    "range": "12am-4am",
+    "rate": 16,
+  }
+]);
+
+const familyC = new Family([
+  {
+    "range": "5pm-9pm",
+    "rate": 21
+  },
+  {
+    "range": "10pm-12am",
+    "rate": 15,
+  },
+]);
---
+++ family.js
@@ -0,0 +1,9 @@
+const convertToInternalTime = require("./helperFunctions");
+
+class Family {
+  constructor(rates){
+    this.rates = rates;
+  }
+}
+
+module.exports = Family;
