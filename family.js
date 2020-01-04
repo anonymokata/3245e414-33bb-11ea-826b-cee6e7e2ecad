e40e834c1e4c90715c419c082ae00807d3d19891
someone <someone>
2020-01-04 12:09:59 -0500

    Created function in Family class on initialization to set the start and end rate times to be stored in internal time. For example, FamilyA has an object with the rate of $15 and between 5pm and 11pm, storing 17, and 23 to be used for iterating later.

--- family.js
+++ family.js
@@ -1,8 +1,31 @@
 const convertToInternalTime = require("./helperFunctions");
 
 class Family {
-  constructor(rates){
+  // rates come in the format of an array of objects
+  /*
+  Example:
+    [
+      {
+      "range": "5pm-11pm",
+      "rate": 15
+    },
+    {
+      "range": "11pm-4am",
+      "rate": 20,
+    }
+  ]
+  */
+  constructor(rates) {
     this.rates = rates;
+    this.createInteralRanges();
+  }
+  // convert ranges from rates to match internal time in an array for iterating
+  createInteralRanges() {
+    for (let i = 0; i < this.rates.length; i++) {
+      const rateObj = this.rates[i];
+      rateObj.startHour = convertToInternalTime(rateObj.range.split("-")[0]);
+      rateObj.endHour = convertToInternalTime(rateObj.range.split("-")[1]);
+    }
   }
 }
 
---
+++ tests/family.test.js
@@ -0,0 +1,66 @@
+const { familyA, familyB, familyC } = require("../families");
+
+describe("Check that internal ranges for each family created an array of their time ranges for iterating", () => {
+  test("Expect Family A to have an array of objects, each containing the string formatted range of rates, pay rate as an integer, and the start and end time of the rates in the internalized format", () =>{
+    expect(familyA.rates).toEqual(
+      [
+        {
+          "range": "5pm-11pm",
+          "rate": 15,
+          "startHour": 17,
+          "endHour": 23
+        },
+        {
+          "range": "11pm-4am",
+          "rate": 20,
+          "startHour": 23,
+          "endHour": 4
+        }
+      ]
+    )
+  });
+
+  test("Expect Family B to have an array of objects, each containing the string formatted range of rates, pay rate as an integer, and the start and end time of the rates in the internalized format", () =>{
+    expect(familyB.rates).toEqual(
+      [
+        {
+          "range": "5pm-10pm",
+          "rate": 12,
+          "startHour": 17,
+          "endHour": 22
+        },
+        {
+          "range": "10pm-12am",
+          "rate": 8,
+          "startHour": 22,
+          "endHour": 24
+        },
+        {
+          "range": "12am-4am",
+          "rate": 16,
+          "startHour": 24,
+          "endHour": 4
+        }
+      ]
+    )
+  });
+
+  test("Expect Family C to have an array of objects, each containing the string formatted range of rates, pay rate as an integer, and the start and end time of the rates in the internalized format", () =>{
+    expect(familyC.rates).toEqual(
+      [
+        {
+          "range": "5pm-9pm",
+          "rate": 21,
+          "startHour": 17,
+          "endHour": 21
+        },
+        {
+          "range": "10pm-4am",
+          "rate": 15,
+          "startHour": 22,
+          "endHour": 4
+        }
+      ]
+    )
+  });
+});
